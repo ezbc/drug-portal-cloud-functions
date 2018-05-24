b811f131aa0793b58854079bfc6f5478370f5477
Elijah Bernstein-Cooper <ezbc@ezbc.me>
2018-05-24 08:46:46 -0400

    working pubsub message consumption

--- publishFda/index.js
+++ publishFda/index.js
@@ -1,16 +1,14 @@
 /* globals exports, require */
 //jshint strict: false
 //jshint esversion: 6
-//const gcs = require('@google-cloud/storage')();
-//const stream = require("stream");
 const unzipper = require("unzipper");
 const JSONStream = require("JSONStream");
 const Storage = require('@google-cloud/storage');
-const readline = require('readline');
-const es = require('event-stream')
+const PubSub = require('@google-cloud/pubsub');
 
 // Instantiates a client
 const storage = Storage();
+const pubsub = PubSub();
 
 function getFileStream (bucket, name) {
   if (!bucket) {
@@ -23,9 +21,53 @@ function getFileStream (bucket, name) {
   return storage.bucket(bucket).file(name).createReadStream();
 }
 
-exports.processZip = (event, callback) => {
-    const pubsubMessage = event.data;
+/**
+ * Publishes a message to a Cloud Pub/Sub Topic.
+ *
+ * @example
+ * gcloud alpha functions call publish --data '{"topic":"[YOUR_TOPIC_NAME]","message":"Hello, world!"}'
+ *
+ *   - Replace `[YOUR_TOPIC_NAME]` with your Cloud Pub/Sub topic name.
+ *
+ * @param {object} req Cloud Function request context.
+ * @param {object} req.body The request body.
+ * @param {string} req.body.topic Topic name on which to publish.
+ * @param {string} req.body.message Message to publish.
+ * @param {object} res Cloud Function response context.
+ */
+exports.publish = (req, res) => {
+  if (!req.body.topic) {
+    res.status(500).send(new Error('Topic not provided. Make sure you have a "topic" property in your request'));
+    return;
+  } else if (!req.body.message) {
+    res.status(500).send(new Error('Message not provided. Make sure you have a "message" property in your request'));
+    return;
+  }
 
+  console.log(`Publishing message to topic ${req.body.topic}`);
+
+  // References an existing topic
+  const topic = pubsub.topic(req.body.topic);
+
+  const message = {
+    data: {
+      message: req.body.message
+    }
+  };
+
+  // Publishes a message
+  return topic.publish(message)
+    .then(() => res.status(200).send('Message published.'))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+      return Promise.reject(err);
+    });
+};
+
+exports.processZip = (event, callback) => {
+    const pubsubMessage = event.data.attributes;
+    console.log(pubsubMessage);
     const name = pubsubMessage.objectId,
       bucket = pubsubMessage.bucketId;
     
@@ -35,28 +77,9 @@ exports.processZip = (event, callback) => {
     file.pipe(unzipper.ParseOne())
     .pipe(JSONStream.parse('results.*'))
     .on('data', function publishRecord(data){
-      console.log(`publish record: ${data}`)
+      console.log(`publish record: ${data.boxed_warning}`)
     })
     .on('end', function handleEnd(data) {
       callback(null, `published`)
     })
-
-    /*
-    let count = 0;
-    const options = {
-      input: file
-    };
-  
-    // Use the readline module to read the stream line by line.
-    readline.createInterface(options)
-      .on('line', (line) => {
-        count += 1;
-      })
-      .on('close', () => {
-        callback(null, `File ${file.name} has ${count} words`);
-      });
-    console.log(`count: ${count}`)
-      console.log(`Hello, ${name}!`);
-    */
-    //callback();
 };

ce5315fa0f19d91a9bac1518cc25721dba54e908
Elijah Bernstein-Cooper <ebernsteincooper@awitechnology.com>
2018-08-20 07:04:39 -0400

    added comment

--- call-publishFda.sh
+++ call-publishFda.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Test a local emulating publishFda instance
 
 while read filename; do
 	echo "publishing file: $filename"
@@ -5,7 +7,7 @@ while read filename; do
 	gcloud pubsub topics publish dataset-fda-status \
         --message '{}' \
         --attribute \
-        "objectGeneration=1531955353874591","eventTime=$date","bucketId=drug_portal","eventType=OBJECT_FINALIZE","notificationConfig=projects/_/buckets/drug_portal/notificationConfigs/4","payloadFormat=JSON_API_V1","objectId=data/fda/$filename"
+        "objectGeneration=1","eventTime=$date","bucketId=drug_portal","eventType=OBJECT_FINALIZE","notificationConfig=projects/_/buckets/drug_portal/notificationConfigs/4","payloadFormat=JSON_API_V1","objectId=data/fda/$filename"
   sleep 20s
   echo ""
 done < $1
